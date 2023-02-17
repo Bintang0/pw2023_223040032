a043c92eb5fd5b7dfd25207a924fc3cde417374a
Bintang0 <bintangarya747@gmail.com>
2023-02-17 11:14:51 +0700

    tugas pw1

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Tugas 1b</title>
+</head>
+
+<body>
+    <?php
+    $nrp = 32;
+    ?>
+
+
+
+
+    <?php
+    // nilai pertama
+    echo "aku adalah angka <b>$nrp</b></br>";
+
+    //hasil dari perkalian
+    echo "Jika aku di kali 5, maka aku sekarang menjadi" . "<b>" . " " . $nrp * 5 . "</b></br>";
+
+    //hasil dari pembagian
+    echo "Jika aku di bagi 2, maka aku sekarang menjadi" . "<b>" . " " . 160 / 2 . "</b></br>";
+
+
+    //hasil dari pertambahan
+    echo "Jika aku ditambah 75, maka aku sekarang menjadi" . "<b>" . " " . 80 + 75 . "</b></br>";
+
+
+    //hasil dari pengurangan
+    echo "Jika aku di kurang 20, maka aku sekarang menjadi" . "<b>" . " " . 155 - 20 . "</b></br>";
+
+
+    ?>
+</body>
+
+</html>
